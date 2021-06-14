3b3ca2a63fce392a7b8648f2fff2af91422c2629
isaac93romano@gmail.com <isaac93romano@gmail.com>
2021-06-14 15:53:17 -0400

    Added many features to the site

--- df-front/src/Screens/ProductScreen.js
+++ df-front/src/Screens/ProductScreen.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Rating from '../components/Rating';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { detailsProduct } from '../actions/productActions';
 
 export default function ProductScreen(props) {
+	const dispatch = useDispatch();
+	const productId = props.match.params.id;
 	const productDetails = useSelector((state) => state.productDetails);
 	const { loading, error, product } = productDetails;
+
+	useEffect(() => {
+		dispatch(detailsProduct(productId));
+	}, [dispatch, productId]);
 
 	return (
 		<div>
